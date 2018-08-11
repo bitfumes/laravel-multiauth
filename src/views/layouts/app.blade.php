3770ced6cf0aab124cf701592f223c069bece185
s-sarthak <sarthak@bitfumes.com>
2018-08-11 22:28:02 +0530

    local

--- src/views/layouts/app.blade.php
+++ src/views/layouts/app.blade.php
@@ -55,8 +55,12 @@
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                                 aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            </a>
 
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/admin/roles">Roles</a>
+                            </div>
+                            
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="/admin/logout" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
