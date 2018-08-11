b5fed8d793c764e0aa25c1d32483f26b4dcee658
s-sarthak <sarthak@bitfumes.com>
2018-08-11 21:40:39 +0530

    new role migration

--- src/database/migrations/2018_03_06_023523_create_roles_table.php
+++ src/database/migrations/2018_03_06_023523_create_roles_table.php
@@ -16,9 +16,6 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
