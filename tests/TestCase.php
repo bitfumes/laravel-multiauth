f8ed6100e556482f73b165d716b15482d90b5bb6
s-sarthak <sarthak@bitfumes.com>
2018-08-11 16:01:54 +0530

    in database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,7 @@ class TestCase extends BaseTestCase
         $this->withoutExceptionHandling();
         $this->artisan('migrate', ['--database' => 'testing']);
         $this->loadLaravelMigrations(['--database' => 'testing']);
-        $this->withFactories(__DIR__.'/../src/factories');
+        $this->withFactories(__DIR__.'/../src/database/factories');
     }
 
     protected function getEnvironmentSetUp($app)
