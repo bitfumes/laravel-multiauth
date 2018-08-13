5800cd1596c9e8dcb2bbfaa725075fbe883c7a69
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-08-13 15:56:55 +0530

    added prefix

--- config/multiauth.php
+++ config/multiauth.php
@@ -1,9 +1,28 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validations
+    |--------------------------------------------------------------------------
+    |
+    | Apply your own validations for register and login of admin.
+    |
+    */
     'validations' => [
         'name' => 'required|max:255',
         'email' => 'required|email|max:255|unique:users',
         'password' => 'required|min:6|confirmed',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Prefix
+    |--------------------------------------------------------------------------
+    |
+    | Use prefix to before the routes of multiauth package.
+    | This way you can keep your admin page secure.
+    | Default : admin
+    */
+    'prefix' => 'admin',
 ];
