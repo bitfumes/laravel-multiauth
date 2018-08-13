34ef1b0428b73c70eb692451655b9765ce0232e9
s-sarthak <sarthak@bitfumes.com>
2018-08-14 02:35:16 +0530

    role validations

--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -34,7 +34,7 @@ class RoleController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate(['role_id' => 'required']);
+        $request->validate(['name' => 'required']);
         Role::create($request->all());
 
         return redirect(route('admin.roles'))->with('message', 'New Role is stored successfully successfully');
@@ -42,7 +42,7 @@ class RoleController extends Controller
 
     public function update(Role $role, Request $request)
     {
-        $request->validate(['role_id' => 'required']);
+        $request->validate(['name' => 'required']);
 
         $role->update($request->all());
 
