acf87ea8fd8421f10237ceea521ccb2fdd6782b4
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-08-13 17:26:40 +0530

    added route names

--- src/views/admin/show.blade.php
+++ src/views/admin/show.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-header">
                     Admin List
                     <span class="float-right">
-                        <a href="/admin/register" class="btn btn-sm btn-success">New Admin</a>
+                        <a href="{{route('admin.register')}}" class="btn btn-sm btn-success">New Admin</a>
                     </span>
                 </div>
                 <div class="card-body">
@@ -24,12 +24,12 @@
                                     @endforeach
                                 </span>
                             <div class="float-right">
-                                <a href="" class="btn btn-sm btn-secondary mr-3" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $admin->id }}').submit();">Delete</a>
-                                <form id="delete-form-{{ $admin->id }}" action="/admin/{{ $admin->id }}" method="POST" style="display: none;">
+                                <a href="#" class="btn btn-sm btn-secondary mr-3" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $admin->id }}').submit();">Delete</a>
+                                <form id="delete-form-{{ $admin->id }}" action="{{ route('admin.delete',[$admin->id]) }}" method="POST" style="display: none;">
                                     @csrf @method('delete')
                                 </form>
                             
-                                <a href="/admin/{{ $admin->id }}/edit" class="btn btn-sm btn-primary mr-3">Edit</a>
+                                <a href="{{route('admin.edit',[$admin->id])}}" class="btn btn-sm btn-primary mr-3">Edit</a>
                             </div>
                         </li>
                         @endforeach
