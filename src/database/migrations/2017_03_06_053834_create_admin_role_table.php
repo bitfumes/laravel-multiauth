31825e3f64607f7540db03ddb12d28837a462931
satheeshdomainindia <satheesh@dreamstudio.in>
2018-10-05 16:48:18 +0530

    Fix Migrate Refresh issue (table "admin_role" already exists) (#32)
    Fix Migrate Refresh issue SQLSTATE[HY000]: General error: 1 table "admin_role" already exists

--- src/database/migrations/2017_03_06_053834_create_admin_role_table.php
+++ src/database/migrations/2017_03_06_053834_create_admin_role_table.php
@@ -30,6 +30,6 @@ class CreateAdminRoleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role_admins');
+        Schema::dropIfExists('admin_role');
     }
 }
